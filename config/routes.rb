013d4c6cf0d292756520bd3568a983faee711a0d
SarÃa Spadari <saraspadari22@gmail.com>
2025-12-03 15:52:48 +0100

    creation de profile.html

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
+get "profile", to: "pages#profile"
+
   resources :pages, only: :new do
     collection do
       get :home
