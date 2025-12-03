6d0deeb87801f701eb507b97dc157d310b97441a
Matteo Garbugli matteo.garbugli@yahoo.it <matteo.garbugli@yahoo.it>
2025-12-03 17:04:49 +0100

    test llm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :devise_controller?
 
   def after_sign_up_path_for(resource)
     user_path(resource)
