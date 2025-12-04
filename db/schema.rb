21c4b5dfe74e4144a237401b7a1db4697de09391
Victor Dejean <victor.dejean@hotmail.fr>
2025-12-04 17:07:25 +0100

    geoloc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_04_133511) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_04_134410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_04_133511) do
     t.string "blurred_photo_url"
     t.datetime "shared_at"
     t.string "address"
+    t.index ["created_at"], name: "index_pings_on_created_at"
+    t.index ["latitude", "longitude"], name: "index_pings_on_latitude_and_longitude"
+    t.index ["shared_at"], name: "index_pings_on_shared_at"
     t.index ["user_id"], name: "index_pings_on_user_id"
   end
 
