3adfa211e24783a3cbd43f9be5dee1caf08e6dc5
Victor Dejean <victor.dejean@hotmail.fr>
2025-12-04 15:58:00 +0100

    Configure queue database for Solid Queue on Heroku

--- config/initializers/solid_queue.rb
+++
@@ -1,5 +0,0 @@
-# Configure Solid Queue to use the primary database
-# instead of a separate queue database
-Rails.application.config.after_initialize do
-  SolidQueue::Record.connects_to database: { writing: :primary, reading: :primary }
-end
