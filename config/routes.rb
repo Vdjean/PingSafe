0bdb148f1bde09684b9363afd2ff5ad2afdc0bf3
SarÃa Spadari <saraspadari22@gmail.com>
2025-12-04 14:58:42 +0100

    numero sara dans le profile

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
       resources :messages, only: :create
     end
 
+  end
     resources :levels, only: [:index, :show]
     resources :rewards, only: [:index, :show]
-  end
 end
