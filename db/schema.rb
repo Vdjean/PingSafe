9a81cfc142e5e0ab11f9dcffa442ce9ca50cd0a1
SarÃa Spadari <saraspadari22@gmail.com>
2025-12-08 14:33:48 +0100

    added address migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_04_134410) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_08_133112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_04_134410) do
     t.datetime "updated_at", null: false
     t.string "blurred_photo_url"
     t.datetime "shared_at"
+    t.string "address"
     t.index ["created_at"], name: "index_pings_on_created_at"
     t.index ["latitude", "longitude"], name: "index_pings_on_latitude_and_longitude"
     t.index ["shared_at"], name: "index_pings_on_shared_at"
